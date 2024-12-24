ef34684c1d13eb7c0cb9afd737b4e9e9101fb028
Bruno Fourcroy <bruno.fourcroy@beamery.com>
2024-12-24 16:58:13 +0100

    Day 20 part 1 tests

--- day-20/index.spec.ts
+++ day-20/index.spec.ts
@@ -2,14 +2,14 @@ import { solveStep1, solveStep2 } from ".";
 
 describe('Day X', () => {
     describe('Step 1', () => {
-        // it('works with the example', async () => {
-        //     const result = await solveStep1(true, 0);
-        //     expect(result).toBe(44);
-        // });
-        // it('provides the right answer', async () => {
-        //     const result = await solveStep1(false, 100);
-        //     expect(result).toBe(1521);
-        // });
+        it('works with the example', async () => {
+            const result = await solveStep1(true, 0);
+            expect(result).toBe(44);
+        });
+        it('provides the right answer', async () => {
+            const result = await solveStep1(false, 100);
+            expect(result).toBe(1521);
+        });
     });
     describe('Step 2', () => {
         it('works with the example', async () => {
